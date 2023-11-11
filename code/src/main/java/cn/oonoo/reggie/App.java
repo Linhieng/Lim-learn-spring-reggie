a8d8c14817d45afb3c1cb19f01f6eefd4ab12822
linhieng <790728740@qq.com>
2023-11-11 11:19:58 +0800

    添加登录过滤器

--- code/src/main/java/cn/oonoo/reggie/App.java
+++ code/src/main/java/cn/oonoo/reggie/App.java
@@ -4,9 +4,11 @@ package cn.oonoo.reggie;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
 
 @Slf4j
 @SpringBootApplication
+@ServletComponentScan // 扫描过滤器
 public class App {
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
