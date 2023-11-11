4713217aa49e83842dca14bfee8d08d95ab49561
linhieng <790728740@qq.com>
2023-11-11 17:15:57 +0800

    分页获取分类+新增分类+更新分类

--- code/src/main/java/cn/oonoo/reggie/controller/CategoryController.java
+++ code/src/main/java/cn/oonoo/reggie/controller/CategoryController.java
@@ -1,9 +1,12 @@
 package cn.oonoo.reggie.controller;
 
+import cn.oonoo.reggie.common.R;
+import cn.oonoo.reggie.entity.Category;
 import cn.oonoo.reggie.service.CategoryService;
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -12,4 +15,46 @@ import org.springframework.web.bind.annotation.RestController;
 public class CategoryController {
 
     private final CategoryService categoryService;
+
+    /**
+     * 新增分类（可以是菜品，也可以是套餐）
+     *
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category) {
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
+    /**
+     * 更新分类
+     *
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody Category category) {
+        categoryService.updateById(category);
+        return R.success("更新分类成功");
+    }
+
+    /**
+     * 分页获取所有菜品和套餐类别
+     * @param curPage
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page<Category>> page(@RequestParam("page") int curPage, int pageSize) {
+        Page<Category> categoryPage = new Page<>(curPage, pageSize);
+
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<Category>()
+                .orderByAsc(Category::getSort);
+
+        categoryService.page(categoryPage, queryWrapper);
+
+        return R.success(categoryPage);
+    }
 }
