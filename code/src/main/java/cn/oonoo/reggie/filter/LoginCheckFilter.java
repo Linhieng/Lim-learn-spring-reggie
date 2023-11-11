ceb31ee5aa1066d47e238e3a1f9659a4c2748cbc
linhieng <790728740@qq.com>
2023-11-11 11:42:03 +0800

    过滤器实现登录校验

--- code/src/main/java/cn/oonoo/reggie/filter/LoginCheckFilter.java
+++ code/src/main/java/cn/oonoo/reggie/filter/LoginCheckFilter.java
@@ -1,6 +1,9 @@
 package cn.oonoo.reggie.filter;
 
+import cn.oonoo.reggie.common.R;
+import com.alibaba.fastjson.JSON;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.util.AntPathMatcher;
 
 import javax.servlet.*;
 import javax.servlet.annotation.WebFilter;
@@ -11,6 +14,10 @@ import java.io.IOException;
 @WebFilter(filterName = "loginCheckFilter", urlPatterns = "/*")
 @Slf4j
 public class LoginCheckFilter implements Filter {
+
+    // 路径匹配器，支持根据通配符进行匹配
+    public static final AntPathMatcher PATH_MATCHER = new AntPathMatcher();
+
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
         HttpServletRequest request = (HttpServletRequest) servletRequest;
@@ -18,7 +25,50 @@ public class LoginCheckFilter implements Filter {
 
         log.info("拦截到请求, {}", request.getRequestURI());
 
-        filterChain.doFilter(request, response);
+        // 1.
+        String requestURI = request.getRequestURI();
 
+        // 可以直接放行的请求路径
+        String[] urlsEnable = new String[]{
+                "/employee/login",
+                "/employee/logout",
+                "/backend/**",
+                "/front/**",
+        };
+
+        // 2. 判断本次请求是否放行
+        boolean match = check(urlsEnable, requestURI);
+
+        // 3.
+        if (match) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 4. 判断是否已登录
+        if (request.getSession().getAttribute("employee") != null) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+
+        // 5.
+        response.getWriter().write(JSON.toJSONString(R.error("NOLOGIN")));
     }
+
+
+    /**
+     * 判断 requestURI 是否包含在 urls 中
+     * @param urls
+     * @param requestURI
+     * @return 如果包含，则返回 true，不包含则返回 false
+     */
+    public boolean check(String[] urls, String requestURI) {
+        for (String url : urls) {
+            if (PATH_MATCHER.match(url, requestURI)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
